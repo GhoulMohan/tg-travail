934019b1b4701a71c65766b1183ec9ef676ab371
GhoulMohan <90193919+GhoulMohan@users.noreply.github.com>
2021-12-16 10:27:57 +0100

    Add files via upload

---
+++ glouton exo.py
@@ -0,0 +1,22 @@
+import random
+##
+##listeObjet = [[7,12], [4,11], [3,8], [3,10]]
+##
+##def tri(objets):
+##    return objets[0]/objets[1]
+##
+##def glouton(objets):
+##    p = 0
+##    liste_final = []
+##    for objets in tri():
+##        if p + objets[1] <= 30:
+##            liste_final.append(objets)
+##            p += objets[1]
+##            
+
+
+
+
+
+        
+            
